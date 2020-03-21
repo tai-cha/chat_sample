cdcef5189c9228f24ed5711e12964f5cd367eee3
taichanne30 <ne300209@senshu-u.jp>
2020-03-21 18:56:22 +0900

    Initial Commit

---
+++ app/models/concerns/post.rb
@@ -0,0 +1,15 @@
+module Post
+
+  def self.table_name_prefix
+    'post_'
+  end
+
+  module Relations
+    extend ActiveSupport::Concern
+
+    included do
+      has_many :room_posts, as: :postable
+      has_many :rooms, through: :room_posts
+    end
+  end
+end
